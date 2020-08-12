f2096a94f7d2f7d074aa88cf2040d71a4154f915
ivybsugarman <840464844@qq.com>
2020-08-12 23:20:41 +0800

    创建了coupon、member、order、product、ware服务

---
+++ gulimall-member/src/test/java/com/ibs/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibs.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
